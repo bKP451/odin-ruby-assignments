92a5252d8a50ca5852afda675fb3b098f1e8b0a5
Bikash Pandey <bkppandey87@gmail.com>
2023-12-27 14:22:11 +0545

    stock picker with each_with_index method

---
+++ stock_picker_each_index.rb
@@ -0,0 +1,29 @@
+def stock_picker(prices)
+  profit = 0
+  lowest_price = prices[0]
+  i = 0
+  index_of_sell_date = 0
+  temp_index_of_buy_date = 0
+  index_of_buy_date = 0
+  
+  prices.each_with_index do |price, index|
+    if price < lowest_price
+      lowest_price = price
+      temp_index_of_buy_date = index
+    end
+
+    current_profit = price - lowest_price
+
+    if current_profit > profit
+      profit = current_profit
+      index_of_sell_date = index
+      index_of_buy_date = temp_index_of_buy_date
+    end
+
+  end
+
+   p [index_of_buy_date, index_of_sell_date]
+
+end
+
+stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
