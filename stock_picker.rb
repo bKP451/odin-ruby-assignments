53761a87f8b85807ebf9b9ce037aa859272c1156
Bikash Pandey <bkppandey87@gmail.com>
2023-12-27 13:47:03 +0545

    stock picker done

--- stock_picker.rb
+++ stock_picker.rb
@@ -11,20 +11,28 @@ def stock_picker(prices)
   profit = 0
   lowest_price = prices[0]
   i = 0
-
+  index_of_sell_date = 0
+  temp_index_of_buy_date = 0
+  index_of_buy_date = 0
+  
   while i < prices.length
     if prices[i] < lowest_price
-      lowest_price = prices[i] 
+      lowest_price = prices[i]
+      temp_index_of_buy_date = i
     end
 
-    if prices[i] - lowest_price > profit
-      profit = prices[i] - lowest_price
+    current_profit = prices[i] - lowest_price
+
+    if current_profit > profit
+      profit = current_profit
+      index_of_sell_date = i
+      index_of_buy_date = temp_index_of_buy_date
     end
     i = i + 1
   end
 
 
-  p profit
+  p [index_of_buy_date, index_of_sell_date]
 end
 
 stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
