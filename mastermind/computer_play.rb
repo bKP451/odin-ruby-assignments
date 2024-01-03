63217191811189f61a862dff06eebaa01f3fe725
Bikash Pandey <bkppandey87@gmail.com>
2024-01-03 12:25:37 +0545

    Display the colored clues for the computer

--- mastermind/computer_play.rb
+++ mastermind/computer_play.rb
@@ -32,6 +32,20 @@ def winning_message_for_computer(computer_cracked_the_code, index)
   puts "Computer guessed your so-called master code."
 end
 
+def color_computer_circle(individual_number, number_index)
+  master_code_for_computer_to_guess = $master_code_for_computer
+  
+  if master_code_for_computer_to_guess.include?(individual_number)
+    if(master_code_for_computer_to_guess[number_index] == individual_number)
+      return ' ○ '.bg_green
+    else
+      return ' ○ '.bg_gray
+    end
+  else 
+    return ' ○ '
+  end  
+end
+
 
 def display_colorized_computer_guess(computer_guessed_code)
   individual_computer_guess_numbers = ''
@@ -39,10 +53,10 @@ def display_colorized_computer_guess(computer_guessed_code)
 
   4.times do |i|
     individual_computer_guess_numbers = individual_computer_guess_numbers + colorize(computer_guessed_code[i]) + "\t"
-    # comparison = comparison + color_circle()
+    comparison = comparison + color_computer_circle(computer_guessed_code[i], i) + "\t"
   end
 
-  puts individual_computer_guess_numbers
+  puts individual_computer_guess_numbers + "\t" + comparison + "\n \n"
 end
 
 
@@ -70,7 +84,7 @@ def let_computer_play(master_code_for_computer)
   i = 0
   loop do
     i = i + 1
-    puts "Iteration #{i}. Computer guesses"
+    puts "Iteration #{i}. Computer guesses \n \n"
     computer_cracked_the_code = let_computer_guess_master_code
     puts "Computer cracked the code is #{computer_cracked_the_code}"
     if computer_cracked_the_code || i == 12
