1feec83f58aa10c756ad94e17e149ad7211830f9
Pandey Bikash <bkppandey87@gmail.com>
2024-01-22 16:29:55 +0545

    Fix linting issues on bubble_sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,28 +1,28 @@
+# frozen_string_literal: true
+
 def bubble_sort(unsorted_array)
   return unsorted_array if unsorted_array.length <= 1
-  i = 0
-  temp = 0
+
   n = unsorted_array.length
-  
-  for i in 1..n-2 do
+
+  (1..n - 2).each do |i|
     single_pass(unsorted_array)
     p "Pass #{i}: #{unsorted_array}"
   end
 
-  return unsorted_array
-
+  unsorted_array
 end
 
 def single_pass(unsorted_array)
   i = 0
-  while i < unsorted_array.length-1
-    if unsorted_array[i] > unsorted_array[i+1]
+  while i < unsorted_array.length - 1
+    if unsorted_array[i] > unsorted_array[i + 1]
       temp = unsorted_array[i]
-      unsorted_array[i] = unsorted_array[i+1]
-      unsorted_array[i+1] = temp
+      unsorted_array[i] = unsorted_array[i + 1]
+      unsorted_array[i + 1] = temp
     end
-    i = i + 1
+    i += 1
   end
 end
 
-p bubble_sort([5, 1, 4, 2, 8])
+p bubble_sort([5, 1, 4, 2, 8, 1])
